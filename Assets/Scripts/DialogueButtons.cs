482265d293e64ce790fd20385e4c692613dfbd0b
Kyle Zhang <kylezzhang@berkeley.edu>
2022-01-11 19:57:15 -0800

    why did the buttons object have those components

---
+++ Assets/Scripts/DialogueButtons.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(LayoutGroup))]
+public class DialogueButtons : MonoBehaviour
+{
+    public Button buttonPrefab;
+    
+    
+}
